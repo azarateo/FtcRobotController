928fa855a85a80d69225dd448099e5f695139f7d
Edwin Alejandro Zarate Orjuela <azarateo@gmail.com>
2024-07-01 17:56:29 +0800

    Running DC at 0.5
    Running the DC motor at 0.5 the speed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1Jul1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1Jul1.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class test1Jul1 {
+@TeleOp
+public class test1Jul1 extends LinearOpMode{
+
+    public DcMotor TheDCMotor   = null;
+    public Servo TheServo = null;
+
+
+    @Override
+    public void runOpMode() {
+
+        TheDCMotor  = hardwareMap.get(DcMotor.class, "drive");
+        TheServo = hardwareMap.get(Servo.class,"servo");
+        TheDCMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        waitForStart();
+        while (opModeIsActive()) {
+            TheDCMotor.setPower(0.5);
+        }
+
+    }
+
 }
