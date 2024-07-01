a86681d40ea810a9db03a13e05b0540efdabc7ad
Edwin Alejandro Zarate Orjuela <azarateo@gmail.com>
2024-07-01 18:30:36 +0800

    Running with internal encoder
    Setting direction, resetting encoder, run using encoder as a set u0p then setting target position, running to position and setting power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1Jul1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test1Jul1.java
@@ -5,8 +5,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp
 public class test1Jul1 extends LinearOpMode{
+
+
 
     public DcMotor TheDCMotor   = null;
     public Servo TheServo = null;
@@ -14,13 +25,26 @@ public class test1Jul1 extends LinearOpMode{
 
     @Override
     public void runOpMode() {
-
         TheDCMotor  = hardwareMap.get(DcMotor.class, "drive");
         TheServo = hardwareMap.get(Servo.class,"servo");
         TheDCMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        TheDCMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        TheDCMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         while (opModeIsActive()) {
-            TheDCMotor.setPower(0.5);
+
+//            using the power for continuos movement at a specific power
+//            TheDCMotor.setPower(0.5);
+
+//            using the encoder to define a position
+            telemetry.addData("encoder position",TheDCMotor.getCurrentPosition());
+            TheDCMotor.setTargetPosition(5000);
+            TheDCMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            TheDCMotor.setPower(0.4);
+            
+                telemetry.addData("encoder position",TheDCMotor.getCurrentPosition());
+                telemetry.update();
+
         }
 
     }
